9ce75782277415125f01905c12b2c4594683f310
dashakozachenko <dkozacenko4@gmail.com>
2022-05-30 16:29:08 +0200

    Remade

--- js/script.js
+++ js/script.js
@@ -1,28 +1,25 @@
 'use strict';
 
-const count = 10;
-let arr = new Array(count);
+(function (){
 
-function fillTable() {
-    for (let i = 0; i < count; ++i) {
-        arr[i] = new Array(count);
-        for (let j = 0; j < count; ++j) {
-            arr[i][j] = i * 10 + j + 1;
+    function fillTable () {
+        const table = document.createElement('table');
+        let row = document.createElement('tr');
+
+        for (let i = 0; i < 10 * 10; i++) {
+            if ((i + 1) % 10 === 0) {
+                let td = document.createElement('td');
+                td.innerHTML = String (i + 1);
+                row.append(td);
+                table.append(row);
+                row = document.createElement('tr');
+            }else {
+                const td = document.createElement('td');
+                td.innerHTML = String (i + 1);
+                row.append(td);
+            }
         }
+        document.body.prepend(table);
     }
-}
-
-fillTable();
-
-function showTable() {
-    for (let i = 0; i < count; ++i) {
-        for (let j = 0; j < count; ++j) {
-            document.write(arr[i][j] +" ");
-        }
-            document.write("<br/>");
-    }
-}
-
-showTable();
-
-console.log(arr);
+    fillTable();
+})();
